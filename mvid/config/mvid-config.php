59a8c709e0c4d47a6fa7cc5fe1b22940205b608e
mikrov <jakob@mikrov.dk>
2012-10-22 10:38:34 +0200

    Config-change

--- mvid/config/mvid-config.php
+++ mvid/config/mvid-config.php
@@ -8,11 +8,11 @@
         /* Type in the shared key you recieved from MV-ID after you registered your application domain. If you havn't
            registered your application domain yet there is a form you can fill in at:
              https://signon.mv-nordic.com/wiki/app-domain-reg */
-        "shared_key" => "426a56307dcb85e1ed6cfb9849c2fe86",
+        "shared_key" => "<shared-key>",
 
         /* The domain you wish to register the user sessions with. The base domain of this value must match the
            application domain you have registered with MV-ID */
-        "domain" => "mv-id-test.valhalla.local",
+        "domain" => "<application-domain>",
 
         "redirect_on_success" => "start.php",
         "redirect_on_failure" => "error.php"),
@@ -23,27 +23,27 @@
            Default: "mv_session_id" */
         "name" => "mv_session_id",
 
-        
+
         /* Choose how to store the mv_session_id:
            1. "cookie"   - If you plan to develope an application that communicates with MV-ID's webservices via javascript
                            you must store the mv_session_id as a session cookie on the client's browser.
-                           
+
            2. "php"      - On the other hand, if yuo are going to do all communications to MV-ID's webservices from the
                            serverside, you might as well store the mv_session_ids as a PHP session variable.
-                           
+
            3. "memcache" - If you have more than one backend webserver and want to do communication with MV-ID's webservices
                            on the serverside you might concider using a memcache server for storing mv_session_ids. That
                            way you can share it between you backend webservers effortlessly.
-                           
+
            4.            - If you want to store your mv_session_id a completely different way you can extend the with a new
                            session storage class by adding it in the directory sessionstorage.
 
            Default: "cookie" */
         "type" => "cookie",
-        
+
         /* Memcache storage type */
         "memcache" => Array(
-          "server" => "10.1.5.139",
+          "server" => "<memcache-host>",
           //"port" => 11211,
           //"timeout" => 3600
         )
