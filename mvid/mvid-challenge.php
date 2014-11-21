d70e19dc020498641f318d7c7fe5ceab2f5a30db
soren-mvnordic <soerenc@mikrov.dk>
2014-11-21 13:02:58 +0100

    Added client features
    Updated jsonwsp client and added possibility to set url, ignore ssl
    warnings and add cookies to the registratino process.

--- mvid/mvid-challenge.php
+++ mvid/mvid-challenge.php
@@ -9,9 +9,15 @@
   //   https://signon.mv-nordic.com/session-security/SessionSecurity
   //
   // This method is a challenge-response mechanism, that ensures that only a registered application
-  // server can create a valid mv_session_id. It takes two arguments:
+  // server can create a valid mv_session_id. It takes the following arguments:
   // 1. The mv_session_hash recieved after successful login.
   // 2. The domain name registered for the application.
+  // 3. Shared key to use to make mv_session_id
+  // 4. mv_session_id the variable that the session id is saved to
+  // 5. A variable where the error_message is stored, if any
+  // 6. A service url if another service besides the standard signon.mv-nordic.com service should be used, if empty the standard url will be used (optional)
+  // 7. If ssl warnings from the webservice should be ignored by the client, set true to ignore (not recommended)
+  // 8. Array of cookies to use send with the registration response
   //
   // The result of this call is a generated nonce value to salt the challenge response. Now there
   // are 3 values known by both MV-ID and the application server: mv_session_hash, nonse and the
@@ -19,9 +25,10 @@
   //
   //   $mv_session_id = md5($mv_session_hash.$nonce.$shared_key);
 
-  function registerSessionUsage($mv_session_hash,$domain,$shared_key,&$mv_session_id,&$error_message) {
+  function registerSessionUsage($mv_session_hash,$domain,$shared_key,&$mv_session_id,&$error_message,$serviceUrl="",$ignoreSSLWarnings=false,$cookies=null) {
+  
     // Connect to MV-ID's session security service to register a valid application session
-    $client = new JsonWspClient("https://signon.mv-nordic.com/session-security/SessionSecurity/jsonwsp/description");
+    $client = new JsonWspClient(trim($serviceUrl) != "" ? $serviceUrl : "https://signon.mv-nordic.com/session-security/SessionSecurity/jsonwsp/description",$ignoreSSLWarnings,$cookies);
     $client->setViaProxy(true);
 
     // Send registration request
